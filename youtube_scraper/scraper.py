4a0cf4d959b299e0b214cec1c5a4f279a64b9086
gaspa93 <mattiagasparini2@gmail.com>
2019-07-01 16:28:20 +0200

    Fixed setup.py for newer pip versions + Fixed extraction of likes and dislikes

--- youtube_scraper/scraper.py
+++ youtube_scraper/scraper.py
@@ -7,29 +7,33 @@ class YoutubeScrape(object):
     def __init__(self, soup):
         """ Initialize and scrape """
         self.soup = soup
-        self.title = self.parse_string('.watch-title')
-        self.poster = self.parse_string('.yt-user-info')
-        self.views = self.parse_int('.watch-view-count')
-        self.published = self.parse_string('.watch-time-text')
+        self.title = self.parse_string('.watch-title', 0)
+        self.poster = self.parse_string('.yt-user-info', 0)
+        self.views = self.parse_int('.watch-view-count', 0)
+        self.published = self.parse_string('.watch-time-text', 0)
         self.published = re.sub(r'(Published|Uploaded) on', '', self.published).strip()
-        self.like = self.parse_int('#watch-like')
-        self.dislike = self.parse_int('#watch-dislike')
+        self.like = self.parse_int('.yt-uix-clickcard', 4)
+        self.dislike = self.parse_int('.yt-uix-clickcard', 5)
 
-    def parse_int(self, selector):
+    def parse_int(self, selector, pos):
         """ Extract one integer element from soup """
-        return int(re.sub('[^0-9]', '', self.parse_string(selector)))
+        return int(re.sub('[^0-9]', '', self.parse_string(selector, pos)))
 
-    def parse_string(self, selector):
+    def parse_string(self, selector, pos):
         """ Extract one particular element from soup """
-        return self.soup.select(selector)[0].get_text().strip()
+        return self.soup.select(selector)[pos].get_text().strip()
 
 
 def scrape_html(html):
     """ Return meta information about a video """
-    return YoutubeScrape(BeautifulSoup(html))
+    return YoutubeScrape(BeautifulSoup(html, 'html.parser'))
 
 
 def scrape_url(url):
     """ Scrape a given url for youtube information """
     html = requests.get(url).text
+
+    #with open('test.html', 'w') as f:
+    #    f.write(html.encode('utf-8'))
+
     return scrape_html(html)

aad5f0abdb1d175802610d316bf0b08b7e67bf1b
gaspa93 <mattiagasparini2@gmail.com>
2019-08-21 14:20:44 +0200

    added pos as kwargs

--- youtube_scraper/scraper.py
+++ youtube_scraper/scraper.py
@@ -7,19 +7,19 @@ class YoutubeScrape(object):
     def __init__(self, soup):
         """ Initialize and scrape """
         self.soup = soup
-        self.title = self.parse_string('.watch-title', 0)
-        self.poster = self.parse_string('.yt-user-info', 0)
-        self.views = self.parse_int('.watch-view-count', 0)
-        self.published = self.parse_string('.watch-time-text', 0)
+        self.title = self.parse_string('.watch-title')
+        self.poster = self.parse_string('.yt-user-info')
+        self.views = self.parse_int('.watch-view-count')
+        self.published = self.parse_string('.watch-time-text')
         self.published = re.sub(r'(Published|Uploaded) on', '', self.published).strip()
         self.like = self.parse_int('.yt-uix-clickcard', 4)
         self.dislike = self.parse_int('.yt-uix-clickcard', 5)
 
-    def parse_int(self, selector, pos):
+    def parse_int(self, selector, pos=0):
         """ Extract one integer element from soup """
         return int(re.sub('[^0-9]', '', self.parse_string(selector, pos)))
 
-    def parse_string(self, selector, pos):
+    def parse_string(self, selector, pos=0):
         """ Extract one particular element from soup """
         return self.soup.select(selector)[pos].get_text().strip()
 
